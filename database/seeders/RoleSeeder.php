b768f82e5404c3e090feaadc3f1e2d958232b618
Ilham Taufiq <ilhamtaufiq97@gmail.com>
2025-12-23 16:02:30 +0700

    feat: Add RoleSeeder to create default admin, user, and operator roles.

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create default roles
+        $roles = ['admin', 'user', 'operator'];
+        
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+        
+        $this->command->info('Default roles created: ' . implode(', ', $roles));
+    }
+}
